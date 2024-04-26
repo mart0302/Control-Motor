3734c0450d8aa2c5699268210957e69aba747e06
Martin Ojeda <88889204+mart0302@users.noreply.github.com>
2024-04-26 08:29:01 -0600

    Update motor_control.cpp

--- motor_control.cpp
+++ motor_control.cpp
@@ -1,5 +1,15 @@
 #include "motor_control.h"
 
+int pinENA = 13;
+int pinIN1 = 5;
+int pinIN2 = 4;
+
+const int frecuencia = 500;
+const int canal_pwm = 0;
+const int resolucion = 8;
+const int velocidad_maxima = 255;
+const int zona_central = 20;
+
 void inicializarMotor() {
   pinMode(pinENA, OUTPUT);
   pinMode(pinIN1, OUTPUT);
@@ -13,29 +23,21 @@ void inicializarMotor() {
 }
 
 void controlarMotor(int valor) {
-  barrita = valor;
+  barrita = String(valor);
 
-  // Convertir el valor del deslizador al rango -127 a 127
-  int barrita_int = barrita.toInt() - 127;
+  int barrita_int = valor - 127;
 
-  // Ajustar la dirección y la velocidad del motor según el valor del deslizador
   if (barrita_int > zona_central) {
-    // A la derecha, girar en una dirección y aumentar la velocidad
     digitalWrite(pinIN1, HIGH);
     digitalWrite(pinIN2, LOW);
-    // Ajustar la velocidad
     ledcWrite(canal_pwm, map(barrita_int, zona_central + 1, 127, velocidad_maxima, 0));
   } else if (barrita_int < -zona_central) {
-    // A la izquierda, girar en la dirección opuesta y aumentar la velocidad
     digitalWrite(pinIN1, LOW);
     digitalWrite(pinIN2, HIGH);
-    // Ajustar la velocidad
     ledcWrite(canal_pwm, map(barrita_int, -127, -zona_central - 1, velocidad_maxima, 0));
   } else {
-    // En el rango central, detener el motor
     digitalWrite(pinIN1, LOW);
     digitalWrite(pinIN2, LOW);
-    // Detener el motor
     ledcWrite(canal_pwm, 0);
   }
 }
